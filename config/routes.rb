a17e8799cadab5a1e375d52bca61648a02f69272
SEED <naokitane6828@gmail.com>
2020-04-20 22:11:36 +0900

    [Finish] 7

--- config/routes.rb
+++ config/routes.rb
@@ -8,4 +8,8 @@ Rails.application.routes.draw do
   get 'todolists' => 'todolists#index'
 
   get 'todolists/:id' => 'todolists#show', as: 'todolist'
+
+  get 'todolists/:id/edit' => 'todolists#edit', as: 'edit_todolist'
+
+  patch 'todolists/:id' => 'todolists#update', as: 'update_todolist'
 end
